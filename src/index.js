1eab6c4d77dc633b1bc7a6257227ceba44b69a42
Dimitri Kopriwa <dka@dev-11.yeutech.vn>
2019-02-07 15:05:58 +0700

    fix(polyfill): do not reject public suffix

--- src/index.js
+++ src/index.js
@@ -40,7 +40,9 @@ export function polyfill(options) {
   if (opts.isomorphicFetch) {
     const Tough = require('tough-cookie');
     const Store = new Tough.MemoryCookieStore();
-    const cookieJar = new Tough.CookieJar(Store);
+
+    const rejectPublicSuffixes = false; // See https://github.com/salesforce/tough-cookie#cookiejarstoreoptions
+    const cookieJar = new Tough.CookieJar(Store, rejectPublicSuffixes);
     const fetch = require('fetch-cookie')(require('isomorphic-fetch'), cookieJar);
     root.fetch = fetch;
     root.cookieJar = cookieJar;
